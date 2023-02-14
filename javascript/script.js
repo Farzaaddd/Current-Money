e85db363e5efc6402082df9f333109fce4eafbab
Farzad Solaimani <farzadsolaimanii@gmail.com>
2023-02-14 19:24:38 +0400

    Added the latest changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,8 @@
 // The main variables we need
 const dropList = document.querySelectorAll(".drop-list select"),
   fromCurrency = document.querySelector(".from select"),
-  toCurrency = document.querySelector(".to select");
+  toCurrency = document.querySelector(".to select"),
+  getBtn = document.querySelector("form button");
 
 // Get the rate and country code
 for (let i = 0; i < dropList.length; i++) {
@@ -31,6 +32,17 @@ function loadFlag(element) {
   }
 }
 
+// Load site
+window.addEventListener("load", (e) => {
+  getExchangeRate();
+});
+
+// Get changes for rate
+getBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  getExchangeRate();
+});
+
 // Swap rate
 let exchangeBtn = document.querySelector(".drop-list .icon");
 exchangeBtn.addEventListener("click", (e) => {
@@ -39,4 +51,30 @@ exchangeBtn.addEventListener("click", (e) => {
   toCurrency.value = tempCode;
   loadFlag(fromCurrency);
   loadFlag(toCurrency);
+  getExchangeRate();
 });
+
+// Get API for current rate
+function getExchangeRate() {
+  const amount = document.querySelector(".amount input"),
+    totalExchangeRateTxt = document.querySelector(".exchange-rate");
+
+  let amountVal = amount.value;
+  if (amountVal == "" || amountVal == "0") {
+    amount.value = "1";
+    amountVal = 1;
+  }
+
+  totalExchangeRateTxt.innerText = "Getting exchange rate...";
+  let url = `https://v6.exchangerate-api.com/v6/YourAPIKEY/latest/${fromCurrency.value}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      let exchangeRate = result.conversion_rates[toCurrency.value];
+      let totalExchangeRate = amountVal * exchangeRate.toFixed(2);
+      totalExchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
+    })
+    .catch(() => {
+      totalExchangeRateTxt.innerText = "Something went wrong !";
+    });
+}
